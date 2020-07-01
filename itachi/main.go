5cac18aee9a4a6b81badf39c4b099864284ea7c0
Karthik Natarajan <karthik.natarajan@avalara.com>
2020-07-01 17:18:22 +0530

    Initial go code with tests and Makefile to run tests inside docker

---
+++ itachi/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"itachi/itachi/config"
+)
+
+func main() {
+	// load application configurations
+	if err := config.LoadConfig("./config"); err != nil {
+		panic(fmt.Errorf("invalid application configuration: %s", err))
+	}
+
+	fmt.Println(config.Config.Name)
+}
